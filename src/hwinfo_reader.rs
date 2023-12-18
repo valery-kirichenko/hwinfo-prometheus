e92ac3f0941ba53b25386caa70e3e3b9833c9cb3
Valery Kirichenko <valera5505@gmail.com>
2023-12-19 02:00:41 +0300

    Refactored gauge and other stuff

--- src/hwinfo_reader.rs
+++ src/hwinfo_reader.rs
@@ -1,16 +1,22 @@
 use std::error::Error;
 use std::fmt::{Display, Formatter};
 
+use tabled::settings::{Alignment, Modify, Panel, Style};
+use tabled::settings::object::Rows;
+use tabled::Table;
 use win_sys::{FILE_MAP_READ, ViewOfFile};
 
 use crate::hwinfo_types::{HWiNFOReadingElement, HWiNFOSensorElement, HWiNFOSharedMemory};
+use crate::table_types::Reading;
+use crate::utf8_to_str;
 
 pub struct Reader<'a> {
-    pub info: HWiNFOSharedMemory,
+    pub info: &'a HWiNFOSharedMemory,
     pub sensors: Vec<&'a HWiNFOSensorElement>,
     pub readings: Vec<&'a HWiNFOReadingElement>,
 
     _view: ViewOfFile,
+    svm_ptr: *const HWiNFOSharedMemory,
     readings_ptr: *const HWiNFOReadingElement,
 }
 
@@ -20,8 +26,8 @@ impl Reader<'_> {
             Ok(mapping) => mapping,
             Err(_) => return Err(SVMOpenError),
         };
-        let view = win_sys::MapViewOfFile(file_mapping.as_handle(), FILE_MAP_READ, 0, 0, 0).unwrap();
-        let svm_ptr = view.as_mut_ptr() as *const HWiNFOSharedMemory;
+        let _view = win_sys::MapViewOfFile(file_mapping.as_handle(), FILE_MAP_READ, 0, 0, 0).unwrap();
+        let svm_ptr = _view.as_mut_ptr() as *const HWiNFOSharedMemory;
         let info = unsafe { &(*svm_ptr) };
         let mut sensors_ptr = unsafe { svm_ptr.add(1) as *const HWiNFOSensorElement };
 
@@ -33,10 +39,11 @@ impl Reader<'_> {
         }
 
         let mut result = Self {
-            info: info.clone(),
+            info,
             sensors,
             readings: Vec::with_capacity(info.reading_elements_number as usize),
-            _view: view,
+            _view,
+            svm_ptr,
             readings_ptr: sensors_ptr as *const HWiNFOReadingElement,
         };
         result.update_readings();
@@ -45,6 +52,9 @@ impl Reader<'_> {
     }
 
     pub fn update_readings(&mut self) {
+        self.info = unsafe { &(*self.svm_ptr) };
+        println!("{:?}", self.info);
+
         let mut readings_ptr = self.readings_ptr;
         self.readings.clear();
         for _ in 0..self.info.reading_elements_number {
@@ -52,6 +62,32 @@ impl Reader<'_> {
             self.readings.push(reading);
             readings_ptr = unsafe { readings_ptr.add(1) };
         }
+    }
+
+    #[allow(unused)]
+    pub fn print_readings(&self) {
+        let mut readings = Vec::with_capacity(self.readings.len());
+        for reading in &self.readings {
+            readings.push(Reading {
+                id: reading.reading_id,
+                sensor_name: utf8_to_str(&self.sensors.get(reading.sensor_index as usize).unwrap().user_name_utf8),
+                label: utf8_to_str(&reading.user_label_utf8),
+                unit: utf8_to_str(&reading.unit_utf8),
+                value: reading.value,
+                min_value: reading.min_value,
+                avg_value: reading.avg_value,
+                max_value: reading.max_value,
+                reading_type: reading.reading_type,
+            });
+        }
+        let table = Table::builder(&readings)
+            .index()
+            .build()
+            .with(Panel::header("Readings"))
+            .with(Style::sharp())
+            .with(Modify::new(Rows::first()).with(Alignment::center()))
+            .to_string();
+        println!("{}", table);
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+// #![windows_subsystem = "windows"]
+
 use std::sync::{Arc, RwLock};
 use std::sync::atomic::AtomicU64;
 use std::thread;
@@ -16,6 +18,7 @@ use crate::hwinfo_types::SensorReadingType;
 
 mod hwinfo_types;
 mod hwinfo_reader;
+mod table_types;
 
 fn utf8_to_str(utf8: &[u8]) -> String {
     std::ffi::CStr::from_bytes_until_nul(utf8).unwrap().to_str().unwrap().to_string()
@@ -31,15 +34,10 @@ type SharedState = Arc<RwLock<AppState>>;
 pub struct HWiNFOLabels {
     pub sensor: String,
     pub reading: String,
-}
-
-#[derive(Clone, Debug, Hash, PartialEq, Eq, EncodeLabelSet)]
-pub struct HWiNFOExtendedLabels {
-    pub sensor: String,
-    pub reading: String,
     pub unit: String,
 }
 
+#[derive(Default)]
 pub struct Metrics {
     temperature: Family<HWiNFOLabels, Gauge<f64, AtomicU64>>,
     voltage: Family<HWiNFOLabels, Gauge<f64, AtomicU64>>,
@@ -48,11 +46,11 @@ pub struct Metrics {
     power: Family<HWiNFOLabels, Gauge<f64, AtomicU64>>,
     clock: Family<HWiNFOLabels, Gauge<f64, AtomicU64>>,
     usage: Family<HWiNFOLabels, Gauge<f64, AtomicU64>>,
-    other: Family<HWiNFOExtendedLabels, Gauge<f64, AtomicU64>>,
+    other: Family<HWiNFOLabels, Gauge<f64, AtomicU64>>,
 }
 
 impl Metrics {
-    pub fn gauge_general_reading(&self, label: HWiNFOLabels, reading_type: SensorReadingType, value: f64) {
+    pub fn gauge_reading(&self, label: HWiNFOLabels, reading_type: SensorReadingType, value: f64) {
         match reading_type {
             SensorReadingType::None => {}
             SensorReadingType::Temp => { self.temperature.get_or_create(&label).set(value); }
@@ -62,43 +60,30 @@ impl Metrics {
             SensorReadingType::Power => { self.power.get_or_create(&label).set(value); }
             SensorReadingType::Clock => { self.clock.get_or_create(&label).set(value); }
             SensorReadingType::Usage => { self.usage.get_or_create(&label).set(value); }
-            SensorReadingType::Other => {}
+            SensorReadingType::Other => { self.other.get_or_create(&label).set(value); }
         };
-    }
-
-    pub fn gauge_other_reading(&self, label: HWiNFOExtendedLabels, value: f64) {
-        self.other.get_or_create(&label).set(value);
     }
 }
 
 #[tokio::main]
 async fn main() {
-    let metrics = Arc::new(RwLock::new(Metrics {
-        temperature: Family::default(),
-        voltage: Family::default(),
-        fan_speed: Family::default(),
-        current: Family::default(),
-        power: Family::default(),
-        clock: Family::default(),
-        usage: Family::default(),
-        other: Family::default(),
-    }));
+    let metrics = Arc::new(RwLock::new(Metrics { ..Default::default() }));
     let shared_state = Arc::new(RwLock::new(AppState {
         registry: <Registry>::default()
     }));
 
-    {
-        let mut state = shared_state.write().unwrap();
-        let metrics = metrics.read().unwrap();
-        state.registry.register("temperature", "Temperature in degrees Celsius", metrics.temperature.clone());
-        state.registry.register("voltage", "Voltage in Volts", metrics.voltage.clone());
-        state.registry.register("fan_speed", "Fan speed in RPM", metrics.fan_speed.clone());
-        state.registry.register("current", "Current in Amps", metrics.current.clone());
-        state.registry.register("power", "Power in watts", metrics.power.clone());
-        state.registry.register("clock", "Clock speed in MHz", metrics.clock.clone());
-        state.registry.register("usage", "Usage in percents", metrics.usage.clone());
-        state.registry.register("other", "Arbitrary value with its own unit", metrics.other.clone());
-    }
+    let mut state = shared_state.write().unwrap();
+    let metrics_read = metrics.read().unwrap();
+    state.registry.register("temperature", "Temperature measurement", metrics_read.temperature.clone());
+    state.registry.register("voltage", "Voltage measurement", metrics_read.voltage.clone());
+    state.registry.register("fan_speed", "Fan speed measurement", metrics_read.fan_speed.clone());
+    state.registry.register("current", "Current measurement", metrics_read.current.clone());
+    state.registry.register("power", "Power measurement", metrics_read.power.clone());
+    state.registry.register("clock", "Clock speed measurement", metrics_read.clock.clone());
+    state.registry.register("usage", "Usage measurement", metrics_read.usage.clone());
+    state.registry.register("other", "Arbitrary value with its own unit", metrics_read.other.clone());
+    drop(state);
+    drop(metrics_read);
 
     let app = Router::new()
         .route("/metrics", get(handler))
@@ -122,22 +107,13 @@ async fn main() {
             for reading in &reader.readings {
                 let sensor = reader.sensors.get(reading.sensor_index as usize).unwrap();
                 let reading_type = reading.reading_type;
-                if reading_type != SensorReadingType::Other {
-                    metrics.read().unwrap().gauge_general_reading(
-                        HWiNFOLabels
-                        {
-                            sensor: utf8_to_str(&sensor.user_name_utf8),
-                            reading: utf8_to_str(&reading.user_label_utf8),
-                        }, reading_type, reading.value);
-                } else {
-                    metrics.read().unwrap().gauge_other_reading(
-                        HWiNFOExtendedLabels
-                        {
-                            sensor: utf8_to_str(&sensor.user_name_utf8),
-                            reading: utf8_to_str(&reading.user_label_utf8),
-                            unit: utf8_to_str(&reading.unit_utf8),
-                        }, reading.value);
-                }
+                metrics.read().unwrap().gauge_reading(
+                    HWiNFOLabels
+                    {
+                        sensor: utf8_to_str(&sensor.user_name_utf8),
+                        reading: utf8_to_str(&reading.user_label_utf8),
+                        unit: utf8_to_str(&reading.unit_utf8),
+                    }, reading_type, reading.value);
             }
             thread::sleep(Duration::from_millis(reader.info.polling_period as u64));
             reader.update_readings();
